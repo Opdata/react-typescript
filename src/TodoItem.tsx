d33b4ede7bb280f52d230269371dc015f5ec6238
Opdata <pb03641@gmail.com>
2021-01-20 17:11:48 +0900

    TodoItem Basic Type, Tag Add

--- src/TodoItem.tsx
+++ src/TodoItem.tsx
@@ -1,7 +1,32 @@
-const TodoItem = ({ value }: any) => {
+import { ReactElement } from 'react';
+import { Item, TodoList } from './Types';
+
+const TodoItem = ({
+  value,
+  setTodoList,
+}: {
+  value: Item;
+  setTodoList: React.Dispatch<React.SetStateAction<TodoList>>; // 체크박스에 사용
+}): ReactElement => {
+  const onRemove = () => {
+    //
+  };
+
   return (
     <li>
-      <b>{value.text}</b>
+      {/* 체크밧스 추가 예정 */}
+      <b
+        style={
+          value.state
+            ? { textDecoration: 'line-through' }
+            : { textDecoration: 'none' }
+        }
+      >
+        {value.text}
+      </b>
+      <button style={{ marginLeft: '10px' }} onClick={onRemove}>
+        삭제
+      </button>
     </li>
   );
 };
