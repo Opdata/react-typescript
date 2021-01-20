99520aa868e2a4ef4171106d2f4060e08a4c6cc5
Opdata <pb03641@gmail.com>
2021-01-20 17:10:41 +0900

    Props Type, Function return Type, onChange Event Type Add

--- src/Label.tsx
+++ src/Label.tsx
@@ -1,18 +1,31 @@
-import { useState } from 'react';
+import { ReactElement, useState } from 'react';
+import { Item, TodoList } from './Types';
 
-const Label = ({ item, setItem, TodoList, setTodoList }: any) => {
+const Label = ({
+  item,
+  setItem,
+  TodoList,
+  setTodoList,
+}: {
+  item: Item;
+  setItem: React.Dispatch<React.SetStateAction<Item>>;
+  TodoList: TodoList;
+  setTodoList: React.Dispatch<React.SetStateAction<TodoList>>;
+}): ReactElement => {
   const [text, setText] = useState('');
   const [number, setNumber] = useState(0);
-  const onChange = (e: any): void => {
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
     const { value } = e.currentTarget;
     setText(value);
   };
 
-  const onSubmit = (e: any) => {
+  const onSubmit = (e: React.FormEvent) => {
+    if (text !== '') {
+      setItem({ id: number, text: text, state: false });
+      setNumber(number + 1);
+      setTodoList(TodoList.concat(item));
+    }
     e.preventDefault();
-    setItem({ id: number, text: text, state: false });
-    setNumber(() => number + 1);
-    setTodoList(TodoList.concat(item));
   };
 
   return (
