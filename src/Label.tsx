87ab55c3c5cfee74cca07352443d914c9813b888
Opdata <pb03641@gmail.com>
2021-01-25 23:39:19 +0900

    Code Refactor(Unecessary Code, onSubmit setState Code Fix)

--- src/Label.tsx
+++ src/Label.tsx
@@ -2,36 +2,43 @@ import { ReactElement, useState } from 'react';
 import { Item, TodoList } from './Types';
 
 const Label = ({
-  item,
-  setItem,
   TodoList,
   setTodoList,
 }: {
-  item: Item;
-  setItem: React.Dispatch<React.SetStateAction<Item>>;
   TodoList: TodoList;
   setTodoList: React.Dispatch<React.SetStateAction<TodoList>>;
 }): ReactElement => {
-  const [text, setText] = useState<string>('');
   const [itemnumber, setitemNumber] = useState<number>(0);
+  const [text, setText] = useState<string>('');
   const onChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
     const { value } = e.currentTarget;
     setText(value);
   };
 
-  const onSubmit = (e: React.FormEvent) => {
-    setItem(() => ({
-      id: itemnumber,
-      text: text,
-    }));
-    setitemNumber((prev: number) => prev + 1);
-    setTodoList(TodoList.concat(item));
+  const onSubmit = (e: React.FormEvent, number: number, str: string) => {
+    const Todo: Item = {
+      id: number,
+      text: str,
+    };
+    setTodoList(TodoList.concat(Todo));
+    setitemNumber(itemnumber + 1);
+    setText('');
+
     e.preventDefault();
   };
 
   return (
-    <form onSubmit={onSubmit}>
-      <input onChange={onChange} value={text}></input>
+    <form
+      onSubmit={(e: React.FormEvent) => {
+        onSubmit(e, itemnumber, text);
+      }}
+    >
+      <input
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+          onChange(e);
+        }}
+        value={text}
+      ></input>
       <button type={'submit'}>추가</button>
     </form>
   );
